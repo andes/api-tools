1b899cce16c3ebca3c7616c70b62feb698c47978
Mariano Andres Botta <marianoabotta@gmail.com>
2019-08-27 12:28:59 -0300

    feat(event-bus): add new test

--- event-bus/src/index.spec.ts
+++ event-bus/src/index.spec.ts
@@ -9,3 +9,14 @@ test('should emit true', () => {
     EventCore.emitAsync('evento', true);
 
 });
+
+test('should emit two params', () => {
+
+    EventCore.on('evento-2', (first: any, second: any) => {
+        expect(first).toBe(true);
+        expect(second).toBe(false);
+    });
+
+    EventCore.emitAsync('evento-2', true, false);
+
+});
