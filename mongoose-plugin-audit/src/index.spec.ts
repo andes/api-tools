16d1a2db349716395baeb23e68242008d4b1f8e1
Mariano Andres Botta <marianoabotta@gmail.com>
2019-09-30 08:10:40 -0300

    test(mongoose-plugin-audit):  add mongo-in-memory for unit test

--- mongoose-plugin-audit/src/index.spec.ts
+++ mongoose-plugin-audit/src/index.spec.ts
@@ -1,13 +1,53 @@
+const mongoose = require('mongoose');
+
 import { AuditPlugin } from './index';
-import * as mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server-global';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
+
+let mongoServer: any;
+beforeAll(async () => {
+
+    mongoServer = new MongoMemoryServer();
+    const mongoUri = await mongoServer.getConnectionString();
+    mongoose.connect(mongoUri);
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+});
 
 test('should emit true', () => {
 
     const schema = new mongoose.Schema({});
     schema.plugin(AuditPlugin);
-    const Model = mongoose.model('prueba', schema);
+    const Model = mongoose.model('prueba2', schema);
     const m = new Model({});
     expect(typeof (m as any).audit).toBe('function');
 
 });
 
+test('should store createdAt field', async () => {
+    const schema = new mongoose.Schema({ documento: String });
+    schema.plugin(AuditPlugin);
+    const Model = mongoose.model('prueba', schema);
+    const m = new Model({ documento: '1010' });
+    m.audit({ user: { usuario: { documento: '123456' } } });
+
+    await m.save();
+
+    const savedModel: any = await Model.findById(m._id);
+    expect(savedModel.createdAt).toBeDefined();
+    expect(savedModel.createdBy.documento).toBe('123456');
+
+    m.documento = 'asdasd';
+    m.audit({ user: { usuario: { documento: '123456' } } });
+    await m.save();
+
+    const savedModel2: any = await Model.findById(m._id);
+    expect(savedModel2.updatedAt).toBeDefined();
+    expect(savedModel2.updatedBy.documento).toBe('123456');
+
+
+});
