bd6df75283f2def68f8bfe2fde42bc57f08fa0ad
condorpiedra <natalia.huenchuman@gmail.com>
2020-06-16 13:43:38 -0300

    Update query-renaper.ts

--- fuentes-autenticas/src/query-renaper.ts
+++ fuentes-autenticas/src/query-renaper.ts
@@ -60,7 +60,7 @@ export function renaperToAndes(ciudadano: any) {
     paciente.genero = ciudadano.sexo || '';
     const fecha = ciudadano.fechaNacimiento ? ciudadano.fechaNacimiento.split('-') : null;
     paciente.fechaNacimiento = (fecha && new Date(fecha[0], fecha[1] - 1, fecha[2]) || null);
-    const fechaFallecido = ciudadano.fallecido && ciudadano.fallecido !== 'NO' ? ciudadano.fechaFallecimiento.split('-') : null;
+    const fechaFallecido = ciudadano.fallecido && ciudadano.fallecido !== 'NO' ? ciudadano.fechaFallecido.split('-') : null;
     paciente.fechaFallecimiento = fechaFallecido && new Date(fechaFallecido[0], fechaFallecido[1] - 1, fechaFallecido[2]) || null;
     paciente.foto = ciudadano.foto;
     paciente.estado = 'validado';
