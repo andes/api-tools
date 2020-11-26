4d05cd726bf96fa1b9d351748a9f62913089c58b
Natalia Huenchuman <natalia.huenchuman@gmail.com>
2020-11-26 14:33:37 -0300

    feat(fuentes-autenticas): nueva conexión a renaper

--- fuentes-autenticas/src/query-renaper.ts
+++ fuentes-autenticas/src/query-renaper.ts
@@ -1,5 +1,6 @@
 import { IDireccion, IUbicacion } from './IPaciente';
 import { soapRequest, RenaperConfig } from './soap-client';
+const fetch = require('node-fetch');
 
 /**
  * Recupera los datos de una persona de renaper. Espera documento y sexo.
@@ -19,6 +20,48 @@ export async function renaper(persona: any, config: RenaperConfig, formatter: (p
         }
     }
     return null;
+
+}
+
+/**
+ * Recupera los datos de una persona de renaper a través del Xroad. Espera documento y sexo.
+ * @param {object} persona Datos de la persona a verificar
+ * @param {function} formatter Función para transformar los datos.
+ */
+export interface XRoadConfig {
+    securityServer: string;
+    client:
+    {
+        xRoadInstance: string,
+        memberClass: string,
+        memberCode: string,
+        subsystemCode: string
+    };
+}
+
+export async function renaperv2(persona: any, config: XRoadConfig, formatter: (persona: any) => any = null) {
+    const url = `${config.securityServer}/r1/roksnet/GOV/71111229/GP-RENAPER/WS_Renaper_Documento_3/${persona.documento}/${persona.sexo}`;
+    const headers = {
+        'Content-type': 'application/json',
+        'X-Road-Client': `${config.client.xRoadInstance}/${config.client.memberClass}/${config.client.memberCode}/${config.client.subsystemCode}`,
+    };
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers
+        });
+        const body = await response.json();
+        const datos = body.resultado1;
+        if (datos && (datos.nombres !== '') && (datos.apellido !== '')) {
+            datos.documento = persona.documento;
+            datos.sexo = persona.sexo;
+            return formatter ? formatter(datos) : datos;
+        }
+        return null;
+    } catch (err) {
+        return null;
+    }
 
 }
 
