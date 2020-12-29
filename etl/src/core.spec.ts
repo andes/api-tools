43d3e1ce38e5f4a77075cbd66023f67cc9f5977c
Mariano Andres Botta <marianoabotta@gmail.com>
2020-12-29 14:43:38 -0300

    feat(etl): class etl

---
+++ etl/src/core.spec.ts
@@ -0,0 +1,107 @@
+import { transform } from './core';
+
+test('should emit true', () => {
+
+    const datos = transform(
+        {
+            functions: {
+                sum: (v: any) => v + 1,
+                sumWithArgs: (v: any, n: number) => v + n,
+                concat: (...args: string[]) => {
+                    return args.join('');
+                }
+            },
+            schemas: {
+                paciente: {
+                    nombre: '$.nombre',
+                    nombre2: '$.nombre',
+                    apellido: '$.apellido',
+                },
+                estadoSisa: {
+                    gato: '$.tipo',
+                }
+            }
+        },
+        {
+            nombre: '$.paciente.nombre',
+            edad: '$.hola',
+            amigo: {
+                nombre: '$.paciente.nombre',
+                edad: '$.hola',
+            },
+            version: 1,
+            array: [{ nombre: '$.paciente.nombre' }],
+            pac: '$.paciente',
+
+            multiple: {
+                $array: {
+                    target: '$.datos',
+                    schema: {
+                        tipo2: '$.tipo'
+                    }
+                }
+            },
+            funcion: {
+                $apply: {
+                    target: '$.hola',
+                    fn: 'sumWithArgs',
+                    args: [10]
+                }
+            },
+            transform: {
+                $transform: {
+                    target: '$.paciente',
+                    schemaName: 'paciente'
+                }
+            },
+            multipleWithTransform: {
+                $array: {
+                    target: '$.datos',
+                    schema: {
+                        $transform: {
+                            target: '$',
+                            schemaName: 'estadoSisa'
+                        }
+                    }
+                }
+            },
+            concat: {
+                $apply: {
+                    fn: 'concat',
+                    args: ['$.paciente.nombre', ' ', '$.paciente.apellido']
+                }
+            },
+        },
+        {
+            hola: 10,
+            paciente: {
+                nombre: 'carlos',
+                apellido: 'botta'
+            },
+            datos: [
+                { tipo: 'ejecucion' },
+                { tipo: 'validado' },
+            ]
+        }
+    );
+
+    expect(datos.nombre).toBe('carlos');
+    expect(datos.edad).toBe(10);
+    expect(datos.amigo.nombre).toBe('carlos');
+    expect(datos.version).toBe(1);
+    expect(datos.array).toHaveLength(1);
+    expect(datos.array[0].nombre).toBe('carlos');
+    expect(datos.pac.nombre).toBe('carlos');
+    expect(datos.multiple).toHaveLength(2);
+
+    expect(datos.multiple[0].tipo2).toBe('ejecucion');
+
+    expect(datos.funcion).toBe(20);
+
+    expect(datos.transform.nombre2).toBe('carlos');
+    expect(datos.concat).toBe('carlos botta');
+    expect(datos.multipleWithTransform[0].gato).toBe('ejecucion');
+    expect(datos.multipleWithTransform[1].gato).toBe('validado');
+
+
+});
---
+++ etl/src/core.ts
@@ -0,0 +1,97 @@
+const { JSONPath } = require('jsonpath-plus');
+
+function extract(path: string, data: any) {
+    const resultado = JSONPath({
+        path,
+        json: data,
+        wrap: false
+    });
+    return resultado;
+
+}
+
+export function transform(ctx: any, schema: any, data: any) {
+
+    if (typeof schema === 'string' && schema.startsWith('$')) {
+        const v = extract(schema, data);
+        return v;
+    } else if (Array.isArray(schema)) {
+
+        const v: any = schema.map(elem => transform(ctx, elem, data));
+        return v;
+
+    } else if (typeof schema === 'object') {
+
+        const isArrayClouse = Object.keys(schema).length === 1 && schema['$array'];
+        const isApplyClouse = Object.keys(schema).length === 1 && schema['$apply'];
+        const isTransformClouse = Object.keys(schema).length === 1 && schema['$transform'];
+
+        if (isArrayClouse) {
+
+            const arrayData = schema['$array'];
+
+            const target = arrayData.target;
+            const subSchema = arrayData.schema;
+
+            const array = extract(target, data);
+            const mapped = array.map((elem: any) => transform(ctx, subSchema, elem));
+
+            return mapped;
+
+        } else if (isApplyClouse) {
+
+            const applyData = schema['$apply'];
+
+            const target = applyData.target;
+            const fnName = applyData.fn;
+            const args: any[] = applyData.args || [];
+
+            const fn = ctx.functions[fnName];
+
+            // [TODO] Si es un string que lo evalue.
+            const args2 = args.map((item: any) => {
+                if (typeof item === 'string' && item.startsWith('$')) {
+                    return extract(item, data);
+                }
+                return item;
+            });
+
+            const argumentos = [];
+            if (target) {
+                const v = extract(target, data);
+                argumentos.push(v);
+            }
+
+            const r = fn.apply(null, [...argumentos, ...args2]);
+
+            return r;
+
+        } else if (isTransformClouse) {
+
+            const transformData = schema['$transform'];
+            const target = transformData.target;
+            const schemaName = transformData.schemaName;
+
+            const schemaDef = ctx.schemas[schemaName];
+
+            const value = extract(target, data);
+
+            const internal: any = transform(ctx, schemaDef, value);
+
+            return internal;
+
+
+        } else {
+            const d = {};
+            Object.keys(schema).forEach(key => {
+                const internal = transform(ctx, schema[key], data);
+                d[key] = internal;
+            });
+            return d;
+        }
+
+
+    } else {
+        return schema;
+    }
+}
